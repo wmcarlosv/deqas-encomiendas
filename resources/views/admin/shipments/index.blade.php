0f30e5072b71398cb1a00fc87b078fd15c6d9244
Carlos Vargas <desarrolloarve@gmail.com>
2022-11-03 18:26:39 -0400

    agregando botones de exportacion

--- resources/views/admin/shipments/index.blade.php
+++ resources/views/admin/shipments/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Entregas')
 
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+@stop
+
 @section('content_header')
     <h1>Entregas</h1>
 @stop
@@ -45,9 +49,21 @@
 @stop
 
 @section('js')
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+
     <script>
         $(document).ready(function(){
-            $("table").DataTable();
+            $("table").DataTable({
+                 dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+                ]
+            });
         });
     </script>
 @stop
